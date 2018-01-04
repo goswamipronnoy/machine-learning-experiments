841928d69eeaf32ede849ee3f42528f6bc583db5
Pronnoy Goswami <Pronnoy_Goswami@mckinsey.com>
2018-01-05 05:03:16 +0530

    Gradient Descent for linear regression

---
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -0,0 +1,15 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+%GRADIENTDESCENT Performs gradient descent to learn theta
+%   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+%   taking num_iters gradient steps with learning rate alpha
+
+% Initialize some useful values
+m = length(y); % number of training examples
+J_history = zeros(num_iters, 1);
+    for iter = 1:num_iters
+        theta0 = theta(1) - (alpha * (1/m) * sum((X * theta - y) .* X(:, 1)));
+        theta1 = theta(2) - (alpha * (1/m) * sum((X * theta - y) .* X(:, 2)));
+        theta = [theta0; theta1];
+        J_history(iter) = computeCost(X, y, theta);
+    end
+end
