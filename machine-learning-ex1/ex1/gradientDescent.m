e13fad9e8f467fdd65d24867194864409e4a4aa4
Pronnoy Goswami <Pronnoy_Goswami@mckinsey.com>
2018-01-05 05:12:53 +0530

    Dummy commit

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -3,7 +3,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-% Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
     for iter = 1:num_iters
