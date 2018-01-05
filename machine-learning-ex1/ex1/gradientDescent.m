b8ab910a86446056fd7dcd941dae0abdb7647f14
Pronnoy Goswami <Pronnoy_Goswami@mckinsey.com>
2018-01-05 05:34:42 +0530

    Checking GPG key with dummy commit again

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -3,7 +3,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
 %   taking num_iters gradient steps with learning rate alpha
 
-m = length(y); % number of training examples in data set
+m = length(y); % number of training examples in the data set
 J_history = zeros(num_iters, 1);
     for iter = 1:num_iters
         theta0 = theta(1) - (alpha * (1/m) * sum((X * theta - y) .* X(:, 1)));
