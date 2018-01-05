05237b44eb6f681f2a541f5264de6935c7e235b4
Pronnoy Goswami <Pronnoy_Goswami@mckinsey.com>
2018-01-05 05:38:26 +0530

    Dummy commit again

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -5,6 +5,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 
 m = length(y); % number of training examples in the data set
 J_history = zeros(num_iters, 1);
+
     for iter = 1:num_iters
         theta0 = theta(1) - (alpha * (1/m) * sum((X * theta - y) .* X(:, 1)));
         theta1 = theta(2) - (alpha * (1/m) * sum((X * theta - y) .* X(:, 2)));
